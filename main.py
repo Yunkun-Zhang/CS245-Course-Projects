ba5909ddd7d2f27f81019dc674c23e0983ccd243
Yunkun-Zhang <ykzhang@sjtu.edu.cn>
2021-03-29 21:52:58 +0800

    VAE 做不出来呜呜呜

--- main.py
+++ main.py
@@ -2,35 +2,59 @@ from dataset import load_data
 from SVM import runSVM
 from selection.Genetic_alg import GA
 from projection.auto_encoder import AE, VAE
+import argparse
 
 # params
-auto_encoder_dim = 16
 svm_C = 5
 svm_k = 'rbf'
+parser = argparse.ArgumentParser()
+parser.add_argument('-m', '--method', default='ae')
+parser.add_argument('-d', '--dim', default=64)
+parser.add_argument('-b', '--batch', default=128)
+args = parser.parse_args()
 
 print('Loading data...')
 X, X_t, y, y_t = load_data()
 
-vae = VAE(auto_encoder_dim)
-vae.build(X.shape[1])
-vae.fit(X, validation_data=X_t)
-X_vae, Xt_vae = vae.predict(X, X_t)
 
-score = runSVM(svm_C, svm_k, X_vae, y, Xt_vae, y_t)
-print("VAE score:", score)
-'''
-print('Training auto encoder...')
-ae = AE(auto_encoder_dim)
-ae.build(X.shape[1])
-ae.auto_encoder.summary()
-ae.fit(X, validation_data=X_t)
-X_ae, Xt_ae = ae.predict(X, X_t)
+def runAE(auto_encoder_dim):
+    print('Training auto encoder...')
+    ae = AE(auto_encoder_dim)
+    ae.build(X.shape[1])
+    ae.auto_encoder.summary()
+    ae.fit(X, validation_data=X_t)
+    X_ae, Xt_ae = ae.predict(X, X_t)
+    score = runSVM(svm_C, svm_k, X_ae, y, Xt_ae, y_t)
+    with open('result.txt', 'a') as f:
+        f.write(f'AE score: {score} (dim={auto_encoder_dim}, kernel={svm_k})\n')
 
-score = runSVM(svm_C, svm_k, X_ae, y, Xt_ae, y_t)
-print("Auto encoder score:", score)
 
-print("Starting GA")
-ga = GA(2048, 2, X, X_t, y, y_t)
-res = ga.update()
-print("final mask: ", res)
-'''
+def runVAE(auto_encoder_dim):
+    print('Training validation auto encoder...')
+    vae = VAE(auto_encoder_dim)
+    vae.build(X.shape[1])
+    vae.auto_encoder.summary()
+    vae.fit(X, validation_data=X_t)
+    X_vae, Xt_vae = vae.predict(X, X_t)
+    score = runSVM(svm_C, svm_k, X_vae, y, Xt_vae, y_t)
+    with open('result.txt', 'a') as f:
+        f.write(f'VAE score: {score} (dim={auto_encoder_dim}, kernel={svm_k})\n')
+
+
+def runGA():
+    print("Starting GA")
+    ga = GA(2048, 2, X, X_t, y, y_t)
+    res = ga.update()
+    score = runSVM(svm_C, svm_k, X[:, res != 0], y, X_t[:, res != 0], y_t)
+    with open('result.txt', 'a') as f:
+        f.write(f'VAE score: {score} (kernel={svm_k})\n')
+
+
+if __name__ == '__main__':
+    args.method = 'vae'
+    if args.method == 'ae':
+        runAE(args.dim)
+    elif args.method == 'vae':
+        runVAE(args.dim)
+    elif args.method == 'ga':
+        runGA()
--- projection/auto_encoder.py
+++ projection/auto_encoder.py
@@ -1,5 +1,4 @@
 import tensorflow as tf
-# tf.compat.v1.disable_eager_execution()
 import tensorflow.keras.backend as K
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Dense, Lambda
@@ -12,33 +11,29 @@ class AE:
         self.auto_encoder = None
         self.encoder = None
 
-    def build(self, input_shape):
-        img = Input(shape=input_shape)
-        shape = input_shape // 2
+    def build(self, input_dim, intermediate_dim=512):
+        img = Input(shape=input_dim)
+        if isinstance(intermediate_dim, int):
+            intermediate_dim = [intermediate_dim]
 
         # encoder
-        encoded = Dense(shape, activation='relu')(img)
-        shape //= 4
-        while shape > self.dim:
-            encoded = Dense(shape, activation='relu')(encoded)
-            shape //= 4
+        encoded = Dense(intermediate_dim[0], activation='relu')(img)
+        for dim in intermediate_dim[1:]:
+            encoded = Dense(dim, activation='relu')(encoded)
         encoder_output = Dense(self.dim, activation='relu')(encoded)
 
         # decoder
-        shape *= 4
-        decoded = Dense(shape, activation='relu')(encoder_output)
-        shape *= 4
-        while shape < input_shape:
-            decoded = Dense(shape, activation='relu')(decoded)
-            shape *= 4
-        decoder_output = Dense(input_shape, activation='sigmoid')(decoded)
+        decoded = Dense(intermediate_dim[-1], activation='relu')(encoder_output)
+        for dim in intermediate_dim[-2::-1]:
+            decoded = Dense(dim, activation='relu')(decoded)
+        decoder_output = Dense(input_dim, activation='sigmoid')(decoded)
 
         self.auto_encoder = Model(inputs=img, outputs=decoder_output)
         self.encoder = Model(inputs=img, outputs=encoder_output)
 
         self.auto_encoder.compile(optimizer='adam', loss='binary_crossentropy')
 
-    def fit(self, X_train, epochs=40, batch_size=128, validation_data=None):
+    def fit(self, X_train, epochs=50, batch_size=128, validation_data=None):
         self.auto_encoder.fit(X_train, X_train, epochs=epochs, batch_size=batch_size,
                               shuffle=True, validation_data=(validation_data, validation_data))
 
@@ -46,29 +41,29 @@ class AE:
         return self.encoder.predict(X_train), self.encoder.predict(X_test)
 
 
-class VAE:
-    def __init__(self, dim=2, batch_size=128):
-        self.dim = dim
-        self.batch_size = batch_size
-        self.auto_encoder = None
-        self.encoder = None
+class VAE(AE):
+    def __init__(self, dim=2):
+        super().__init__(dim)
         self.generator = None
+        tf.compat.v1.disable_eager_execution()
 
-    def build(self, original_dim, intermediate_dim=512, epsilon_std=1.0):
-        x = Input(batch_shape=(self.batch_size, original_dim))
+    def build(self, input_dim, intermediate_dim=512, epsilon_std=1.0):
+        x = Input(shape=(input_dim,))
         h = Dense(intermediate_dim, activation='relu')(x)
+        # compute mean and variance of p(Z|X)
         z_mean = Dense(self.dim)(h)
-        z_log_sigma = Dense(self.dim)(h)
+        z_log_var = Dense(self.dim)(h)
 
         def sampling(args):
             z_mean, z_log_sigma = args
-            epsilon = K.random_normal(shape=(self.batch_size, self.dim), stddev=epsilon_std)
-            return z_mean + K.exp(z_log_sigma) * epsilon
+            epsilon = K.random_normal(shape=(self.dim,), stddev=epsilon_std)
+            return z_mean + K.exp(z_log_sigma / 2) * epsilon
 
-        z = Lambda(sampling, output_shape=(self.dim,))([z_mean, z_log_sigma])
+        z = Lambda(sampling, output_shape=(self.dim,))([z_mean, z_log_var])
 
+        # decoder
         decoder_h = Dense(intermediate_dim, activation='relu')
-        decoder_mean = Dense(original_dim, activation='sigmoid')
+        decoder_mean = Dense(input_dim, activation='sigmoid')
         h_decoded = decoder_h(z)
         x_decoded_mean = decoder_mean(h_decoded)
 
@@ -82,15 +77,11 @@ class VAE:
         self.generator = Model(decoder_input, _x_decoded_mean)
 
         def vae_loss(x, x_decoded_mean):
-            xent_loss = binary_crossentropy(x, x_decoded_mean)
-            kl_loss = - 0.5 * K.mean(1 + z_log_sigma - K.square(z_mean) - K.exp(z_log_sigma), axis=-1)
+            xent_loss = K.binary_crossentropy(x, x_decoded_mean)
+            kl_loss = - 0.5 * K.mean(1 + z_log_var - K.square(z_mean) - K.exp(z_log_var), axis=-1)
             return xent_loss + kl_loss
 
         self.auto_encoder.compile(optimizer='rmsprop', loss=vae_loss, experimental_run_tf_function=False)
 
-    def fit(self, X_train, epochs=40, validation_data=None):
-        self.auto_encoder.fit(X_train, X_train, epochs=epochs, batch_size=self.batch_size,
-                              shuffle=True, validation_data=(validation_data, validation_data))
-
-    def predict(self, X_train, X_test):
-        return self.encoder.predict(X_train), self.encoder.predict(X_test)
+    def fit(self, X_train, epochs=50, batch_size=128, validation_data=None):
+        AE.fit(self, X_train, epochs=epochs, batch_size=batch_size, validation_data=validation_data)

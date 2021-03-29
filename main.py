25b93c6328e46888c756650402292ac3435dad52
Caplimbo <zhhqu0305@gmail.com>
2021-03-29 16:33:18 +0800

    球球二巨帮我跑跑呜呜呜

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from dataset import load_data
 from SVM import runSVM
 from auto_encoder import AE
+from selection.Genetic_alg import *
 
 # params
 auto_encoder_dim = 64
@@ -9,7 +10,7 @@ svm_k = 'rbf'
 
 print('Loading data...')
 X, X_t, y, y_t = load_data()
-
+'''
 print('Training auto encoder...')
 ae = AE(auto_encoder_dim)
 ae.build(X.shape[1])
@@ -21,5 +22,11 @@ print('Training SVM...')
 score = runSVM(svm_C, svm_k, X, y, X_t, y_t)
 print("Score:", score)
 
+
 with open('result.txt', 'a') as f:
     f.write(f'{auto_encoder_dim}, /4, {svm_C}: {score}\n')
+'''
+print("starting GA")
+ga = GA(2048, 2048, X, X_t, y, y_t)
+res = ga.update()
+print("final mask: ", res)
